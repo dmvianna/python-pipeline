c9e74e2fb5c7fc285f1c7eea5db4cff47cbade8c
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-05-26 15:09:37 +1000

    add example given where we know the correct answer

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,3 +52,49 @@ def frames() -> Frames:
         }.items()
     }
     return Frames(**frame_dict)
+
+
+@pytest.fixture
+def example_frames() -> Frames:
+    """
+    Frames using case where the correct response is known.
+    """
+    disbursements = {
+        "sgc_amount": [500, 500, 500],
+        "payment_made": map(pd.Timestamp, ["2025-2-17", "2025-3-30", "2025-4-30"]),
+        "pay_period_from": ["", "", ""],
+        "pay_period_to": ["", "", ""],
+        "employee_code": [0, 0, 0],
+    }
+    payslips = {
+        "payslip_id": [1, 1, 1, 2, 2, 3, 3],
+        "end": map(
+            pd.Timestamp,
+            [
+                "2025-01-01",
+                "2025-01-01",
+                "2025-01-01",
+                "2025-02-01",
+                "2025-02-01",
+                "2025-03-01",
+                "2025-03-01",
+            ],
+        ),
+        "employee_code": [0, 0, 0, 0, 0, 0, 0],
+        "code": ["Salary", "Overtime", "Super", "Salary", "Super", "Salary", "Super"],
+        "amount": [5000, 1500, 475, 5000, 475, 5000, 475],
+    }
+    paycodes = {
+        "pay_code": ["Salary", "Overtime", "Super"],
+        "ote_treatment": ["OTE", "Not OTE", "Not OTE"],
+    }
+
+    frame_dict = {
+        k: pd.DataFrame.from_dict(sheet)
+        for k, sheet in {
+            "disbursements": disbursements,
+            "payslips": payslips,
+            "paycodes": paycodes,
+        }.items()
+    }
+    return Frames(**frame_dict)
